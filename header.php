23a9673f89619d711a9f4db41a3b33fc6dd78d24
mick90207 <b98502030@ntu.edu.tw>
2014-09-05 22:23:56 +0800

    header fix

--- header.php
+++ header.php
@@ -9,18 +9,20 @@
                  <li><a href="show_item.php">商品總覽</a></li>
                  <li><a href="show_item_wanted.php">需求總覽</a></li>
                  <li class="dropdown" id="twohandedmenu">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="show_item.php">會員註冊/登入<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
+                        
                        
 <?php //已經登入
 session_start();
 if (isset($_SESSION['MM_Username'])){ ?>
+<a class="dropdown-toggle" data-toggle="dropdown" href="show_item.php">會員專區<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
                             <li><a href="modify.php">修改資料</a></li>
                             <li><a href="management.php">管理商品</a></li>
 							<li><a href="message_area.php">私人訊息</a></li>
                             <li><a href="logout.php">登出</a></li>
 <?php //尚未登入
-} else{ ?>
+} else{ ?><a class="dropdown-toggle" data-toggle="dropdown" href="show_item.php">會員註冊/登入<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
                         	<li><a href="registration.php">會員註冊</a></li>
                             <li><a href="login.php">會員登入</a></li> 
                             <?php }?>

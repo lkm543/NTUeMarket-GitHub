da2a8e069239f79b8c345382677251ecc2f0b3f1
mick90207 <b98502030@ntu.edu.tw>
2014-09-12 17:00:28 +0800

    delete php mailer

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <? include("header.php");?>
-<center>
 	<div id="main">
     
 		<div class="container">
 			<div id="slideshow">
-				<img src="images/slideshow/slideshow-1.jpg" alt="Slideshow Image" />
-				<img src="images/slideshow/slideshow-2.jpg" alt="Slideshow Image" />
-				<img src="images/slideshow/slideshow-3.jpg" alt="Slideshow Image" />
-				<img src="images/slideshow/slideshow-4.jpg" alt="Slideshow Image" />
+				<img src="images_index/slideshow/slideshow-1.jpg" alt="Slideshow Image" />
+				<img src="images_index/slideshow/slideshow-2.jpg" alt="Slideshow Image" />
+				<img src="images_index/slideshow/slideshow-3.jpg" alt="Slideshow Image" />
+				<img src="images_index/slideshow/slideshow-4.jpg" alt="Slideshow Image" />
 			</div><!-- // end #slideshow -->
 			<div class="intro">
-				<p>我們試圖建立一個屬於台大人的二手交換電子商務平台</p>
+				<p>這是讓你和妳互通有無的網站</p>
 			</div><!-- // end .intro -->
-     
+            
+            <center>
 			<div id="about" class="clearfix">
+            
 				<div class="column">
 					<h2>二手書/教科書</h2>
-					<div class="image"><img src="img/article/article-thumb-1.jpg" alt="Article Image" /></div>
+					<div class="image"><img src="images_index/article/article-thumb-1.jpg" alt="Article Image" /></div>
 					<p>二手書/教科書二手書/教科書二手書/教科書二手書/教科書二手書/教科書二手書/教科書二手書/教科書二手書/教科書</p>
 				</div>
 				<div class="column">
 					<h2>穿的</h2>
-					<div class="image"><img src="img/article/article-thumb-2.jpg" alt="Article Image" /></div>
+					<div class="image"><img src="images_index/article/article-thumb-2.jpg" alt="Article Image" /></div>
 					<p>穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿穿</p>
 				</div>
 				<div class="column">
 					<h2>用的</h2>
-					<div class="image"><img src="img/article/article-thumb-3.jpg" alt="Article Image" /></div>
+					<div class="image"><img src="images_index/article/article-thumb-3.jpg" alt="Article Image" /></div>
 					<p>用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用用</p>
 				</div>
 				<div class="column last">
 					<h2>玩的</h2>
-					<div class="image"><img src="img/article/article-thumb-4.jpg" alt="Article Image" /></div>
+					<div class="image"><img src="images_index/article/article-thumb-4.jpg" alt="Article Image" /></div>
 					<p>玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩玩</p>
 				</div>
+            
 			</div><!-- // end #articles -->
+            </center>
 	
         </div>
-			</div>
-    </center>
-    <!-- // end #main -->
-</div>
+	</div><!-- // end #main -->
 <? include("footer.php");?>

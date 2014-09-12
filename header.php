c1e801bde00c2d319395c83144df6002b5620a01
mick90207 <b98502030@ntu.edu.tw>
2014-09-12 20:22:58 +0800

    index fix,change location

--- header.php
+++ header.php
@@ -5,34 +5,14 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="description" content="我們試圖建立一個屬於台大人的二手交換電子商務平台，你可能會有想要買/賣/贈送的二手教科書，要搬家出清的家具、用不到的衣服雜物。你可以藉由社群成員具有需求同質性高、地利之便的優勢，很快找到買/賣家、很方便遞交/接收物品，最重要的是能讓物盡其用，每一分資源都不被浪費。" />
-    <meta name="keywords" content="台大,二手物,交換平台,電子商務" />
-    <title>NTUeMarket</title>
-    <link href="css/style.css" rel="stylesheet" type="text/css" />	
-	<link href="css/nivo-slider.css" rel="stylesheet" type="text/css" />
-    <!--[if IE]><link href="css/style-ie.css" rel="stylesheet" type="text/css" /><![endif]-->
-    <!-- jQuery (使用Bootstrap的JavaScript外掛) -->
-    <script src="//code.jquery.com/jquery.js"></script>
-    <!-- jQuery (使用Bootstrap的JavaScript外掛) -->
-	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
-	<script type="text/javascript">
-		$(window).load(function() {
-			$('#slideshow').nivoSlider({
-				directionNav: false
-			});
-		});
-	</script>
-        <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="css/style-ie.css" rel="stylesheet" type="text/css" />
-    <!-- HTML5 shim 和 Respond.js 讓IE8支援HTML5元素和媒體查詢 -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-    <!-- 在下面加入所有已編譯外掛，或是當需要時加入獨立檔案 -->
-    <script src="js/bootstrap.min.js"></script>
+    <title>CollegeBazarr</title>
+    <!-- Bootstrap!-->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Bootstrap!-->
 </head>
 
 <body>
@@ -41,7 +21,7 @@ session_start();
 	<div id="header">
 		<div class="container">
 			<h1>
-				<a href="default.php" title="NTUeMarket">NTUeMarket</a>
+				<a href="/" title="CollegeBazaar">CollegeBazaar</a>
 		  </h1>
 		  <ul class="menu fr">
                  <li><a href="upload_area.php">上傳商品</a></li>
@@ -67,7 +47,6 @@ if (isset($_SESSION['MM_Username'])){ ?>
                             <?php }?>
                                                     </ul>
                     </li>
-				<!--<li><a href="contact-us.php">聯絡我們</a></li>!-->
                 <li><a href="contact_us.php">聯絡我們</a></li>
 		  </ul>
 		</div>

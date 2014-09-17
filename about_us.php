3be80e94dd1f7c1d585aabe24704cbe969bd18b5
Johnny Bee <johnny78716@hotmail.com>
2014-09-18 01:05:25 +0800

    added about page, delete footer menu items

---
+++ about_us.php
@@ -0,0 +1,7 @@
+<?php include("header.php"); ?>
+<div id="main">
+	<div style="text-align:center; font-size: 25px; margin: 200px">
+<strong>Comming Soon!</strong>
+</div></div>
+
+<?php include("footer.php"); ?>
--- footer.php
+++ footer.php
@@ -1,11 +1,8 @@
 <div id="footer">
 	<div class="container">
 		<ul class="menu fl">
-                 <li><a href="upload_area.php">上傳商品</a></li>
-                 <li><a href="upload_wanted.php">公開徵求</a></li>
-                 <li><a href="show_item.php">商品總覽</a></li>
-                 <li><a href="show_item_wanted.php">需求總覽</a></li>
-		 <li><a href="contact_us.php">聯絡我們</a></li>
+            <li><a href="about_us.php">關於CollegeBazaar</a></li>
+		 	<li><a href="contact_us.php">聯絡我們</a></li>
                 </ul>
 		<p>&copy; CollegeBazaar | <a href="http://collegebazaar.tw/">collegebazaar.tw</a></p>
 	</div>

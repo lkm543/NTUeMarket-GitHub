13bf1a27429a26a250c99025269dc604374f0bf8
mick90207 <b98502030@ntu.edu.tw>
2014-09-22 00:34:12 +0800

    fb like/share in header

--- footer.php
+++ footer.php
@@ -1,11 +1,14 @@
 <div id="footer">
 	<div class="container">
-		<div class="col-md-4">
-		<ul class="menu fl">
-            <li><a href="about_us.php">關於CollegeBazaar</a></li>
-		 	<li><a href="contact_us.php">聯絡我們</a></li>
-        </ul></div>
-		<div class="col-md-4 col-md-offset-4"><p>&copy; CollegeBazaar | <a href="http://collegebazaar.tw/">collegebazaar.tw</a></p></div>
+		<div class="col-md-4" style="float:left;">
+			<ul class="menu fl">
+				<li><a href="about_us.php">關於CollegeBazaar</a></li>
+				<li><a href="contact_us.php">聯絡我們</a></li>
+			</ul>
+		</div>	
+		<div class="col-md-4 col-md-offset-4">
+			<p>&copy; CollegeBazaar | <a href="http://collegebazaar.tw/">collegebazaar.tw</a></p>
+		</div>
 	</div>
 </div>
 </body>

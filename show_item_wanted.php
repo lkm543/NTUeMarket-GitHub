997fde07fcfbaedfdb1024830a23f945a18c5727
mick90207 <b98502030@ntu.edu.tw>
2014-10-01 12:15:38 +0800

    search bar update

--- show_item_wanted.php
+++ show_item_wanted.php
@@ -1,180 +1,173 @@
 <? include("header.php");?>
-<div id="main">
-    <div class="row">
-
-      <div class="col-md-1">
-
-      </div>
-
-      <div class="col-md-2" align="left">
-       
-        <div id="search">
-
-          <?php
-          $sort=$_GET['sort'];
-          ?>
-          <form role="form" action="show_item_wanted.php" method="get">
-            <div class="form-group">
-              <label for="InputKeyword">關鍵字搜尋</label>
-              <input type="text" class="form-control" placeholder="關鍵字" size="10" name="keyword" value="<?php echo $_GET['keyword'];?>">  
-              <input type="hidden" name="sort" value="<?php echo $sort;?>">  
-            </div>
-            <p><button type="submit" class="btn btn-default">Submit</button></p>
-          </form>
-          <table class="table table-hover" width="100%" style="margin:0 0 0 0">
-            <tr><td class="active" onClick="location.href='show_item_wanted.php'"><center>需求總覽</center></td></tr>
-            <tr><td onClick="location.href='show_item.php'"><center>商品總覽</center></td></tr>
-          </table>
-          <table width="100%">
-              <tr><td>
-                <table class="table table-hover" width="100%" style="margin:0 0 0 0">
-                  <tr><td <?php 
-                  if($sort=="life")
-                   echo "class=\"success\" ";
-                 ?>onClick="location.href='show_item_wanted.php?sort=life'"><center>生活用品</center></td></tr>
-                 <tr><td <?php 
-                 if($sort=="clothes")
-                   echo "class=\"success\" ";
-                 ?>onClick="location.href='show_item_wanted.php?sort=clothes'"><center>&nbsp;&nbsp;衣&nbsp;&nbsp;&nbsp;物&nbsp;&nbsp;</center></td></tr>
-                 <tr><td <?php 
-                 if($sort=="bike")
-                   echo "class=\"success\" ";
-                 ?>onClick="location.href='show_item_wanted.php?sort=bike'"><center>&nbsp;腳&nbsp;踏&nbsp;車&nbsp;</center></td></tr>
-                 <tr><td <?php 
-                 if($sort=="book")
-                   echo "class=\"success\" ";
-                 ?>onClick="location.href='show_item_wanted.php?sort=book'"><center>課外讀物</center></td></tr></table>
-               </td>
-               <td>
-                <table class="table table-hover" width="100%" style="margin:0 0 0 0">
-                  <tr><td <?php 
-                  if($sort=="stationary")
-                   echo "class=\"success\" ";
-                 ?>onClick="location.href='show_item_wanted.php?sort=stationary'"><center>&nbsp;&nbsp;文&nbsp;&nbsp;&nbsp;具&nbsp;&nbsp;
-               </center></td></tr>
-               <tr><td <?php 
-               if($sort=="3c")
-                 echo "class=\"success\" ";
-               ?>onClick="location.href='show_item_wanted.php?sort=3c'"><center>&nbsp;&nbsp;3C產品&nbsp;</center></td></tr>
-               <tr><td <?php 
-               if($sort=="textbook")
-                 echo "class=\"success\" ";
-               ?>onClick="location.href='show_item_wanted.php?sort=textbook'"><center>&nbsp;&nbsp;教科書&nbsp;&nbsp;</center></td></tr>
-               <tr><td <?php 
-               if($sort=="else")
-                 echo "class=\"success\" ";
-               ?>onClick="location.href='show_item_wanted.php?sort=else'"><center>&nbsp;&nbsp;其&nbsp;&nbsp;&nbsp;他&nbsp;&nbsp;</center></td></tr>
-             </table>
-            </td></tr>
-          </table>
-
-        </div>
-      </div>
-
-      <div class="col-md-8">
-        <?php 
-        $sort=$_GET['sort'];
-        $page=$_GET['page'];
-        $keyword=$_GET['keyword'];
-        include_once("mysql_info.php");
-        if($sort==null&$keyword==null){
-          $sql = "select * from item_wanted where status=1";
-        }
-        if($sort==null&$keyword!=null){
-          $sql = "select * from item_wanted where (name like '%$keyword%' or detail like '%$keyword%') and status=1";
-        }
-        if($sort!=null&$keyword==null){
-          $sql = "select * from item_wanted where sort='$sort' and status=1";
-        }
-        if($sort!=null&$keyword!=null){
-          $sql = "select * from item_wanted where ((name like '%$keyword%' or detail like '%$keyword%') and sort='$sort') and status=1)";
-        }
-        $result = mysqli_query($link,$sql); // 執行SQL查詢引
-        $total_records=mysqli_num_rows($result);  // 取得記錄數
-        $per = 15; //每頁顯示項目數量 
-        $pages = ceil($total_records/$per); //總頁數
-        if(!isset($_GET["page"])){ 
-            $page=1; //設定起始頁 
-          } 
-          else { 
-            $page = intval($_GET["page"]); //確認頁數只能夠是數值資料 
-            $page = ($page > 0) ? $page : 1; //確認頁數大於零 
-            $page = ($pages > $page) ? $page : $pages; //確認使用者沒有輸入太神奇的數字 
-          }
-          $start = ($page-1)*$per; 
-        //for test
-        //echo $start.$per; 
-          if($sort==null&$keyword==null){
-            $sql2 = "select * from item_wanted where status=1 ORDER BY id DESC limit ".$start.",".$per;
-        $result2 = mysqli_query($link,$sql2); // 執行SQL查詢
-        }
-        if($sort==null&$keyword!=null){
-          $sql2 = "select * from item_wanted where (name like '%$keyword%' or detail like '%$keyword%') and status=1 ORDER BY id DESC limit ".$start.",".$per;
-        $result2 = mysqli_query($link,$sql2); // 執行SQL查詢
-        }
-        if($sort!=null&$keyword==null){
-          $sql2 = "select * from item_wanted where sort='$sort' and status=1 ORDER BY id DESC  limit ".$start.",".$per;
-        $result2 = mysqli_query($link,$sql2); // 執行SQL查詢
-        }
-        if($sort!=null&$keyword!=null){
-          $sql2 = "select * from item_wanted where ((name like '%$keyword%' or detail like '%$keyword%') and sort='$sort') and status=1 ORDER BY id DESC  limit ".$start.",".$per;
-        $result2 = mysqli_query($link,$sql2); // 執行SQL查詢
-        }
-
-
-        ?>
-        <center><div style="margin:20px 0px 5px 0px"><font color="#FF0000" size="5"><?php echo $notice;?></font></div></center>
-        <center>
-        <table>
-        <?php
-        $number_of_row=mysqli_num_rows($result2);
-        $totalCount = ceil($number_of_row/3)*3;
-        for($k = 0; $k < $totalCount; $k ++) {
-
-            if($k%3 == 0) { echo '<tr class="row item_list_row">'; }
-
-            if($row = mysqli_fetch_array($result2)) {
-                    echo '<td class="col-xs-9 col-md-4 col-md-offset-1">
-                          <div class="item_wrapper">
-                          <a href="show_wanted_detail.php?id='.$row['id'].'"><div class="item_title" style="min-height: 30px;">'.$row[name].'</div></a>
-                          <div>需求描述：'.$row[detail].'</div>
-                          <div class="item_value">徵求價格：$'.$row[price].'</div>
-                          <div>上架日期：'.$row[date].'</div>
-                          </div></td>';
-            }
-            else {
-                    echo '<td style="width:230px"></td>';
-            }
-
-            if($k%3 == 2) { echo '</tr><tr><td style="height: 20px;"></td></tr>'; }
-
-        }
-
-          ?>
-
-</table>
-<div class="col-md-12">
-<?php for($i=1;$i<=$pages;$i++) { 
-  echo '<a href="show_item.php?';?>
-  <?php 
-  if($sort==null&&$keyword!=null){
-    echo 'keyword='.$keyword;
-  }
-  if($sort!=null&&$keyword==null){
-    echo 'sort='.$sort;
-  }
-  if($sort!=null&&$keyword!=null){
-    echo 'keyword='.$keyword.'sort='.$sort;
-  }
-  ?>
-  <?php echo 'page='.$i.'"><span class="item_pagination">'.$i.'</span></a>'; 
-}
+<?php 
+$sorting=$_GET['sorting'];
+$sort=$_GET['sort'];
+$page=$_GET['page'];
+$keyword=$_GET['keyword'];
+?><?php 
+$sorting=$_GET['sorting'];
+$sort=$_GET['sort'];
+$page=$_GET['page'];
+$keyword=$_GET['keyword'];
 ?>
+<div id="main" style="padding-top:85px;">
+  <center>
+    <div class="row">
+      <form role="form" action="show_item_wanted.php" method="get">
+        <div class="search_bar col-md-2 col-md-offset-2" style="padding-right:3px;">
+          <input type="text" class="form-control" placeholder="關鍵字" size="10" name="keyword" <?php if($keyword!=NULL) echo "value=".$keyword;?>>
+        </div>
+        <div class="search_bar col-md-1"style="padding-left:5px;padding-right:0px;">
+          <select name="sort" id="sort" class="form-control">
+            <option value="" <?php if($sort=='') echo 'selected=selected'; ?>>商品分類</option>
+            <option value="life" <?php if($sort=='life') echo 'selected=selected'; ?>>生活用品</option>
+            <option value="stationary" <?php if($sort=='stationary') echo 'selected=selected'; ?>>文具</option>
+            <option value="clothes" <?php if($sort=='clothes') echo 'selected=selected'; ?>>衣物</option>
+            <option value="3c" <?php if($sort=='3c') echo 'selected=selected'; ?>>3c產品</option>
+            <option value="bike" <?php if($sort=='bike') echo 'selected=selected'; ?>>腳踏車</option>
+            <option value="book" <?php if($sort=='book') echo 'selected=selected'; ?>>課外讀物</option>
+            <option value="textbook" <?php if($sort=='textbook') echo 'selected=selected'; ?>>教科書</option>
+            <option value="else" <?php if($sort=='else') echo 'selected=selected'; ?>>其他</option>
+          </select>
+        </div>
+        <div class="search_bar col-md-2"style="padding-left:0px;padding-right:0px;">
+          <select name="sorting" id="sort" class="form-control" style="width:75%">
+            <option value="" <?php if($sorting=='') echo 'selected=selected'; ?>>排序方式</option>
+            <option value="date" <?php if($sorting=='date') echo 'selected=selected'; ?>>上架時間：近到遠</option>
+            <option value="date_revert" <?php if($sorting=='date_revert') echo 'selected=selected'; ?>>上架時間：遠到近</option>
+            <option value="price" <?php if($sorting=='price') echo 'selected=selected'; ?>>價格：便宜到貴</option>
+            <option value="price_revert" <?php if($sorting=='price_revert') echo 'selected=selected'; ?>>價格：貴到便宜</option>
+
+          </select>
+        </div>
+        <div class="search_bar col-md-1"style="padding-left:0px;padding-right:80px;">
+          <button type="submit" class="btn btn-default">搜尋</button>
+        </div>
+        <div class="search_bar col-md-3"style="padding-left:0px;text-align:left;">
+          <?php 
+          include_once("mysql_info.php");
+          if($sort==null&$keyword==null){
+            $sql = "select * from item_wanted where status=1";
+          }
+          if($sort==null&$keyword!=null){
+            $sql = "select * from item_wanted where (name like '%$keyword%' or detail like '%$keyword%') and status=1";
+          }
+          if($sort!=null&$keyword==null){
+            $sql = "select * from item_wanted where sort='$sort' and status=1";
+          }
+          if($sort!=null&$keyword!=null){
+            $sql = "select * from item_wanted where ((name like '%$keyword%' or detail like '%$keyword%') and sort='$sort') and status=1";
+          }
+$result = mysqli_query($link,$sql); // 執行SQL查詢引
+$total_records=mysqli_num_rows($result);  // 取得記錄數
+$per = 20; //每頁顯示項目數量 
+$pages = ceil($total_records/$per); //總頁數
+if(!isset($_GET["page"])){ 
+    $page=1; //設定起始頁 
+  } 
+  else { 
+    $page = intval($_GET["page"]); //確認頁數只能夠是數值資料 
+    $page = ($page > 0) ? $page : 1; //確認頁數大於零 
+    $page = ($pages > $page) ? $page : $pages; //確認使用者沒有輸入太神奇的數字 
+  }
+  $start = ($page-1)*$per; 
+  switch ($sorting) {
+
+    case 'date':
+    $order_by="ORDER BY id DESC";
+    break;
+    
+    case 'date_revert':
+    $order_by="ORDER BY id ASC";
+    break;
+
+    case 'price':
+    $order_by="ORDER BY price asc";
+    break;
+
+    case 'price_revert':
+    $order_by="ORDER BY price DESC";
+    break;
+
+    default:
+    $order_by="ORDER BY id DESC";
+    break;
+  }
+
+//for test
+//echo $start.$per; 
+  if($sort==null&$keyword==null){
+    $sql2 = "select * from item_wanted where status=1 ".$order_by." limit ".$start.",".$per;
+$result2 = mysqli_query($link,$sql2); // 執行SQL查詢
+}
+if($sort==null&$keyword!=null){
+  $sql2 = "select * from item_wanted where (name like '%$keyword%' or detail like '%$keyword%') and status=1 ".$order_by." limit ".$start.",".$per;
+$result2 = mysqli_query($link,$sql2); // 執行SQL查詢
+}
+if($sort!=null&$keyword==null){
+  $sql2 = "select * from item_wanted where sort='$sort' and status=1 ".$order_by." limit ".$start.",".$per;
+$result2 = mysqli_query($link,$sql2); // 執行SQL查詢
+}
+if($sort!=null&$keyword!=null){
+  $sql2 = "select * from item_wanted where ((name like '%$keyword%' or detail like '%$keyword%') and sort='$sort') and status=1 ".$order_by." limit ".$start.",".$per;
+$result2 = mysqli_query($link,$sql2); // 執行SQL查詢
+}
+$number_of_row=mysqli_num_rows($result2);
+$totalCount = ceil($number_of_row/4)*4;
+//每頁起始資料序號
+//$result2 = mysqli_query('SELECT * FROM table LIMIT ' . $start . ', ' . $per); 
+//while($data2 = mysql_fetch_row($result2)) { 
+//    print_r($data2); 
+//}
+
+?>
+<span style="font-size:23px;margin-left:20px;">有<span style="color:red;"><?php echo $total_records;?></span>項商品符合需求</span>
+</div>
+</form>
+</div>
+
+
+<!-- 商品展示-->
+<div class="row" style="padding-top:10px;">
+  <div class="col-md-10 col-md-offset-1">
+
+    <center><div style="margin:10px 0px 5px 0px"><font color="#FF0000" size="5"><?php echo $notice;?></font></div></center>
+
+<table>
+  <?php
+  $number_of_row=mysqli_num_rows($result2);
+  $totalCount = ceil($number_of_row/4)*4;
+  for($k = 0; $k < $totalCount; $k ++) {
+
+    if($k%4 == 0) { echo '<tr class="row item_list_row">'; }
+
+    if($row = mysqli_fetch_array($result2)) {
+      echo '<td class="col-xs-9 col-md-3 col-md-offset-1">
+      <div class="item_wrapper">
+        <a href="show_wanted_detail.php?id='.$row['id'].'"><div class="item_title" style="min-height: 30px;">'.$row[name].'</div></a>
+        <div>需求描述：'.$row[detail].'</div>
+        <div class="item_value">徵求價格：$'.$row[price].'</div>
+        <div>上架日期：'.$row[date].'</div>
+      </div></td>';
+    }
+    else {
+      echo '<td style="width:279px"></td>';
+    }
+
+    if($k%4 == 3) { echo '</tr><tr><td style="height: 20px;"></td></tr>'; }
+
+  }
+
+  ?>
+
+    </table>
+    <?php for($i=1;$i<=$pages;$i++) { 
+      echo "<a href=\"show_item_wanted.php?keyword=".$keyword."&sort=".$sort."&sorting=".$sorting."&page=".$i."\"><span class=\"item_pagination\">".$i."</span></a>"; 
+    }
+    ?>
 
 </div>    
-        </div>
-        </div>
-       </center>
-  </div><!-- // end #main -->
 </div>
+</center>
+</div><!-- // end #main -->
 <? include("footer.php");?>

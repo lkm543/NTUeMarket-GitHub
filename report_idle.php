fc46ec6b1d5ef3729ab56e680719ffa1ab9c10bc
mick90207 <b98502030@ntu.edu.tw>
2014-11-21 20:27:51 +0800

    report_bug
    when reporter=owner
    delete directly

--- report_idle.php
+++ report_idle.php
@@ -11,42 +11,55 @@ if(isset($_POST['id'])&&isset($_POST['item_type'])){
     include_once("mysql_info.php");
 
     //檢查登入
-    if($username!=NULL){
-        if($type=="sell")
+if($username!=NULL){
+
+    if($type=="sell")
         {$result2=mysqli_query($link,"select * from item_forsell where id='$id'");}
-            
-        elseif($type=="wanted")
+
+    elseif($type=="wanted")
         {$result2=mysqli_query($link,"select * from item_wanted where id='$id'");}
-            
-        
-        $row = mysqli_fetch_array($result2);
-        //初始
-        if($row[idle]==NULL){
-            $tempt=array();
-            array_push($tempt,$user_id);
-            $tempt=serialize($tempt);
-            if($type=="sell")
+    //直接下架
+    $row = mysqli_fetch_array($result2);
+
+    //初始
+    if($row[idle]==NULL){
+        $tempt=array();
+        array_push($tempt,$user_id);
+        $tempt=serialize($tempt);
+        if($type=="sell")
             {mysqli_query ($link,"update item_forsell set idle='$tempt' where id='$id'");}
-            
-            elseif($type=="wanted")
+
+        elseif($type=="wanted")
             {mysqli_query ($link,"update item_wanted set idle='$tempt' where id='$id'");}
-            
-        }else{  
+
+    }else{  
             //興趣清單裡已有值
-            $tempt=unserialize($row[idle]);
+        $tempt=unserialize($row[idle]);
             //檢查是否已經加入
-            if(!in_array($user_id,$tempt)){  
+        if(!in_array($user_id,$tempt)){  
                 array_push($tempt,$user_id);//增加
                 $tempt=serialize($tempt);
-            if($type=="sell")
-            {mysqli_query ($link,"update item_forsell set idle='$tempt' where id='$id'");}
-            
-            elseif($type=="wanted")
-            {mysqli_query ($link,"update item_wanted set idle='$tempt' where id='$id'");}
+                if($type=="sell")
+                    {mysqli_query ($link,"update item_forsell set idle='$tempt' where id='$id'");}
+
+                elseif($type=="wanted")
+                    {mysqli_query ($link,"update item_wanted set idle='$tempt' where id='$id'");}
             }
         }
-
         $notice='已回報閒置/已成交，待審核中，謝謝您的熱心協助';
+
+    if($row['owner']==$user_id)
+    {
+         if($type=="sell")
+        {mysqli_query($link,"update item_forsell set status=2 where id='$id'");}
+
+        elseif($type=="wanted")
+        {mysqli_query($link,"update item_wanted set status=2 where id='$id'");}
+        $notice='已下架，謝謝您的主動回報';
+
+    }
+
+
 
         if($type=="sell"){
             //商品
